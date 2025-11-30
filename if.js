899ffbde7bdab6de232ed2fd17b2aca1ad4faaf5
yossi shapira <yosi105009@gmail.com>
2025-11-30 16:48:27 +0200

    end if

---
+++ if.js
@@ -0,0 +1,141 @@
+// const score = 85;
+// if (score > 60){
+//     console.log("Passed the test")
+// }
+
+// const temperature = 82;
+// if (temperature > 30){
+//     console.log("It is hot today")
+// }
+
+// const nomber = 14;
+// if (nomber %2 ==0){
+//     console.log("The number is even")
+// }
+
+// const balance = -5;
+// if (balance < 0){
+//     console.log("Warning: negative balance")
+// }
+
+// const score = 120;
+// if (score > 100){
+//      console.log("High score bonus")
+// }
+
+// const inputText = "";
+// if (inputText == ""){
+//     console.log("No input provided")
+// }
+
+// const age = 21;
+// if (age >18){
+//     console.log("User is an adult")
+// }
+
+// const speed = 95;
+// if (speed >90){
+//     console.log("Speeding!")
+// }
+
+// const stock = 0;
+// if (stock === 0){
+//     console.log("Out of stock")
+// } 
+
+// const grade = 93;
+// if (grade >90){
+//     console.log("Grade A")
+// }
+
+const score = 58;
+if (score > 60){
+    console.log("pass")
+}
+else{
+    console.log("Fail")
+}
+
+// const number = 11;
+// if (number %2 ===0){
+//     console.log( "Even number")
+// }
+// else{
+//     console.log("Odd number")
+// }
+
+const age = 10;
+if (age <12){
+    console.log("Child price")
+}
+else{
+    console.log("Adult price")
+}
+
+const speed = 50;
+if (speed >80){
+    console.log("Too fast")
+}
+else{
+    console.log("Speed OK")
+}
+
+const username = "admin";
+if (username === "admin"){
+    console.log("Login successful")
+}
+else{
+    console.log("Login failed")
+}
+
+const number = 0;
+if (number >0){
+    console.log("Positive")
+}
+else{
+   console.log("Zero or negative") 
+}
+
+const stock = 5;
+if (stock > 0){
+    console.log("Item available") 
+}
+else{
+    console.log("Item sold out")
+}
+
+const message = "hello";
+if (message.length > 3){
+     console.log( "Long text")
+}
+else{
+    console.log("Short text")
+}
+
+const password = 1234;
+if (password === 9999){
+     console.log( "Access granted")
+}
+else{
+    console.log("Wrong password")
+}
+
+const value = 7;
+if (value >0 && value <=10){
+     console.log("Inside range")
+}
+else{
+    console.log("Outside range")
+}
+
+
+const grade = 88;
+if (grade > 90){
+    console.log("A")
+}
+else if(grade > 80){
+    console.log("B")
+}
+else{
+    console.log("C or below")
+}
---
+++ loops.js
@@ -0,0 +1,11 @@
+// let counter = 1;
+// while(counter <=100){
+//     console.log(counter)
+//     counter += 1
+// }
+
+for (let i =0; i<=50;i++){
+    if (i %3 === 0){
+        console.log(i)
+    }
+}
