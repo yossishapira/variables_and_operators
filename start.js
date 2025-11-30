1033d54295159706eed7bca383d674aefff610d1
yossi shapira <yosi105009@gmail.com>
2025-11-30 12:26:47 +0200

    end

---
+++ start.js
@@ -0,0 +1,25 @@
+
+const age  = 29;
+const name1 = 'yossi';
+const address = 'Bnei Brak';
+const AreYouVegan = false;
+const NumberOfKids = 0;
+
+
+console.log(
+    `name: ${name1} 
+age: ${age} 
+address: ${address} 
+AreYouVegan: ${AreYouVegan} 
+NumberOfKids: ${NumberOfKids}`);
+
+console.log(
+    `name: ${typeof(name1)}
+age: ${typeof(age)}
+address: ${typeof(address)} 
+AreYouVegan: ${typeof(AreYouVegan)}
+NumberOfKids: ${typeof(NumberOfKids)}`);
+
+const c = true
+console.log(typeof(c))
+console.log(c)
---
+++ variables_and_operators.js
@@ -0,0 +1,148 @@
+let a = 5;
+a = 8;
+console.log(a);
+
+let b = 12;
+b = b +3;
+console.log(b);
+
+const c = 4;
+console.log(c);
+
+// const d = 10;
+// d = 20;
+// console.log(d)
+// שגיאה
+
+var e = 2;
+e = 7;
+console.log(e);
+
+var f = 100;
+var f = 200;
+console.log(f);
+
+let g = 3;
+g = g*2;
+console.log(g)
+
+// let h = 15;
+// let h = 20;
+// console.log(h)
+// שגיאה
+
+const i = 9;
+console.log(i+1);
+
+const j = 5; 
+j + 5;
+console.log(j)
+
+let x = 2;
+let y = 3;
+x = x + y;
+console.log(x);
+
+const aa = 6;
+let bb = 2;
+bb = aa+bb;
+console.log(bb)
+
+let m = 10;
+m = m - 4;
+console.log(m);
+
+var r = 3;
+r = r *5;
+console.log(r);
+
+let q = 4;
+q = q+q;
+console.log(q);
+
+const k = 7
+console.log(k*2);
+
+// const z = 1;
+// z = z+5;
+// console.log(z);
+// שגיאה
+
+let t = 8;
+t = 8 + 2;
+console.log(t);
+t = t + 10;
+console.log(t);
+
+var w = 5;
+var w = w + 5;
+console.log(w);
+
+let p = 10;
+p = p/2;
+console.log(p);
+
+console.log(5+2);
+console.log(10-3);
+console.log(4*6);
+console.log(20/5);
+console.log(15%4);
+console.log(2**3);
+console.log(9+2);
+console.log(12-7);
+console.log(6*6);
+console.log(30/10);
+console.log("5"+2);
+console.log("5"-2);
+console.log(5*"2");
+console.log("10"+"3");
+console.log("10"*"3");
+console.log(true+5);
+console.log(false+7);
+console.log(null+4);
+console.log(undefined+3);
+console.log("hello"-5);
+
+console.log(5<10);
+console.log(5>10);
+console.log(5==5);
+console.log(5===5);
+console.log(7>=3);
+console.log(3<=7);
+console.log(8!=2);
+console.log(8!==2);
+console.log(12===12);
+console.log(20>10);
+console.log("5"==5);
+console.log("5"===5);
+console.log("10">"2");
+console.log("abc"<"abd");
+console.log(true == false);
+console.log(true == 1);
+console.log(false == 0);
+// console.log(nall == undefined);
+// שגיאה
+console.log(null == 0);
+// console.log(undefind == "");
+// שגיאה
+
+console.log(true && true);
+console.log(true && false);
+console.log(true || false);
+console.log(false || false);
+console.log(true || true);
+console.log(1&&0);
+console.log(1||0);
+console.log(10&&5);
+console.log(10||5);
+console.log(0&&5);
+console.log("hello"&&"world");
+console.log("hello"||"world");
+console.log(""&&"text");
+console.log(""||"text");
+console.log(null||"value");
+console.log(null&&"value");
+console.log(undefined||10); 
+console.log(undefined&&10); 
+console.log(false||"yes"); 
+console.log("test"&&false); 
